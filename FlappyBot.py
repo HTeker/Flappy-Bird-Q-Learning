59a9ffd4105663558a2e38296df14ab6bf50bb18
HTeker <h.teker.developer@gmail.com>
2018-06-16 14:40:15 +0200

    Select action based on highest Q-value. If the Q-value for the state does not exist, create a new record in the Q-values and do a random action

--- FlappyBot.py
+++ FlappyBot.py
@@ -18,5 +18,23 @@ class FlappyBot():
             file.write('{}')
 
     def act(self, horizontal_difference, vertical_difference, player_velocity):
-        print("Horizontal difference: {} Vertical difference: {} Player velocity: {}".format(horizontal_difference, vertical_difference, player_velocity))
-        return random.randint(0,1)
+        state = self.convert_states_to_string(horizontal_difference, vertical_difference, player_velocity)
+
+        print(state)
+
+
+        try:
+            # Check which action is better to perform based on the Q-values
+            if self.q_values[state][0] >= self.q_values[state][1]:
+                self.last_action = 0
+                return 0
+            else:
+                self.last_action = 1
+                return 1
+        except:
+            # Create q-value if it does not exist among the q-values
+            self.q_values[state] = [0, 0]
+            return random.randint(0, 1)
+
+    def convert_states_to_string(self, horizontal_difference, vertical_difference, player_velocity):
+        return str(int(horizontal_difference)) + '_' + str(int(vertical_difference)) + '_' + str(player_velocity)
