333e8876a7f1433ad30f3009d5f641577abcb927
HTeker <h.teker.developer@gmail.com>
2018-06-16 14:27:24 +0200

    Loading q-values or creating q-values file

--- FlappyBot.py
+++ FlappyBot.py
@@ -4,29 +4,17 @@ from itertools import chain
 
 class FlappyBot():
     def __init__(self):
+
+
         self.q_values = {}
         self.load_q_values()
-
-        if self.q_values == {}:
-            self.initialize_q_values()
-
-    def initialize_q_values(self):
-        print('Creating and initializing q_values.json file...')
-        for x in chain(list(range(-40, 140, 10)), list(range(140, 421, 70))):
-            for y in chain(list(range(-300, 180, 10)), list(range(180, 421, 60))):
-                for v in range(-10, 11):
-                    self.q_values[str(x) + '_' + str(y) + '_' + str(v)] = [0, 0]
-
-        fd = open('q_values.json', 'w')
-        json.dump(self.q_values, fd)
-        fd.close()
 
     def load_q_values(self):
         try:
             file = open('q_values.json', 'r')
+            self.q_values = json.load(file)
         except IOError:
-            return
-        self.q_values = json.load(file)
+            open('q_values.json', 'w')
 
     def act(self, horizontal_difference, vertical_difference, player_velocity):
         print("Horizontal difference: {} Vertical difference: {} Player velocity: {}".format(horizontal_difference, vertical_difference, player_velocity))
